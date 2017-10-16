9f5185aba96a5669924ccaa292773d2c52eb0b46
xueye2017 <2953661480@qq.com>
2017-10-16 19:16:52 +0800

    move

---
+++ unitywork/Assets/scripts/move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public GameObject cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cube.transform.Translate (new Vector3(1,1,1));
+	}
+}
